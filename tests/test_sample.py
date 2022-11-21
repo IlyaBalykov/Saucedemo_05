0d9e8571fbe78d97e1f1685ce79e86d21962044d
Ilya Balykov <balykov.ilya.88@gmail.com>
2022-11-21 21:18:45 +0300

    feat: add sample_test & add action

---
+++ tests/test_sample.py
@@ -0,0 +1,14 @@
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+def test_sample():
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    driver.get('https://www.saucedemo.com/')
+
+    assert driver.title == 'Swag Labs'
+
+    driver.quit()
+
+# Create pytest.ini for parameters
