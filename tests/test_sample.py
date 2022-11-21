774c66d8ecc6e236fd1b9e7bf05729546c2f616d
Ilya Balykov <balykov.ilya.88@gmail.com>
2022-11-21 22:57:12 +0300

    fix: lint issues in test_sample.py

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
+
 def test_sample():
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     driver.get('https://www.saucedemo.com/')
